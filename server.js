907aba170106f961da68603cbae2cc7acc7723f1
WengCF <becka.070424@gmail.com>
2021-01-21 18:59:43 +0800

    fix some bug about deployment

---
+++ server.js
@@ -0,0 +1,69 @@
+require("dotenv-defaults").config();
+import { GraphQLServer, PubSub } from "graphql-yoga";
+import Query from "./server/resolvers/Query.js";
+import Mutation from "./server/resolvers/Mutation.js";
+import Subscription from "./server/resolvers/Subscription.js";
+
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 80;
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+
+const bodyParser = require("body-parser");
+const apiRoute = require("./src/route/api");
+app.use("/api", apiRoute);
+app.use(bodyParser.json());
+
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
+console.log("Server Ready!");
+
+const Restaurant = require("./server/models/restaurant.js");
+
+const pubsub = new PubSub();
+
+if (!process.env.MONGO_URL) {
+  console.error("Missing MONGO_URL!!!");
+  process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const server = new GraphQLServer({
+  typeDefs: "./server/schema.graphql",
+  resolvers: {
+    Query,
+    Mutation,
+    Subscription,
+  },
+  context: {
+    Restaurant,
+    pubsub,
+  },
+});
+
+db.on("error", (error) => {
+  console.error(error);
+});
+
+db.once("open", () => {
+  console.log("MongoDB connected!");
+});
+
+server.start({ port: 4000 }, () => {
+  console.log("The server is up on port 4000!");
+});
